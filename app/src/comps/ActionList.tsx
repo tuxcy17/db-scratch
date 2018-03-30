f1184a8eabf5e8dc0cf84924f19db48550e3a87f
Cyril RICHARD <tuxcy17@gmail.com>
2018-03-30 22:56:43 +0200

    Defining basic action constant & IHM

--- app/src/comps/ActionList.tsx
+++ app/src/comps/ActionList.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
+import {Action, basicActions} from '../models/Action';
 
 class ActionList extends React.Component<any, any> {
-    state: any = {
-        actions: [
 
-        ]
+    state: any = {
+        actions: basicActions
     };
 
     constructor(props: any) {
@@ -12,10 +12,18 @@ class ActionList extends React.Component<any, any> {
     }
 
     render() {
+        const listActions = this.state.actions.map((action: Action, index: number) =>
+            <li key={index}>
+                <a onClick={action.fn}>
+                    {action.name}
+                </a>
+            </li>
+        );
+
         return (
             <div>
                 <ul>
-
+                    {listActions}
                 </ul>
             </div>
         );
--- app/src/models/Action.ts
+++ app/src/models/Action.ts
@@ -1,6 +1,29 @@
 class Action {
+    state: boolean;
+    name: string;
+    fn: any;
 
-    constructor() {
+    constructor(name: string, state: boolean = false, fn: any) {
+        this.name = name;
+        this.state = state;
+        this.fn = fn;
+    }
 
+    public resetState () {
+        this.state = false;
     }
 }
+
+const basicActions = [
+  new Action("INSERT", false, function () {
+      alert('INSERT');
+  }),
+  new Action("UPDATE", false, function () {
+      alert('UPDATE');
+  }),
+  new Action("DELETE", false, function () {
+      alert('DELETE');
+  })
+];
+
+export {basicActions, Action}
