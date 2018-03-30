69e3051d5bf4e8c8038252da5db457a3e35232b4
Cyril RICHARD <tuxcy17@gmail.com>
2018-03-30 22:56:21 +0200

    removing unused style

--- app/src/comps/App.tsx
+++ app/src/comps/App.tsx
@@ -2,19 +2,18 @@ import * as React from 'react';
 import '../styles/App.css';
 import {TriggerList} from './TriggerList';
 import {TriggerEdition} from './TriggerEdition';
-
-const logo = require('../styles/logo.svg');
+import {ActionList} from './ActionList';
 
 class App extends React.Component<any, any> {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">DB scratch</h1>
                 </header>
                 <TriggerEdition defaultValue="TEST DEFAULT"/>
                 <TriggerList/>
+                <ActionList />
             </div>
         );
     }
